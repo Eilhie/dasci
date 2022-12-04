b9fbf92c7b9c62a12ffa107619e8d1b01ef0cceb
Eilhie <marvinsachio89@gmail.com>
2022-12-04 20:17:22 +0700

    Prototype

--- A_Prototype/File-Testing/Ftest.c
+++ A_Prototype/File-Testing/Ftest.c
@@ -8,8 +8,14 @@ void swap_str(char **x, char **y){
     *y = temp;
 }
 
-void swap(long int *x, long int *y){
-    long int temp = *x;
+void swap_chr(char *x, char *y){
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void swap(double *x, double *y){
+    double temp = *x;
     *x = *y;
     *y = temp;
 }
@@ -19,34 +25,42 @@ int main(){
     FILE *fp;
     fp = fopen("testdata.in", "r");
 
-    char *name[100], *title[100];
-    long int view[100];
-    char ntemp[1010], ttemp[1010];
+    char *town[100], type[100];
+    double temperature[100];
+    char ntemp[1010];
     int i = 0;
+    double sort[100];
+
     while (!feof(fp)) {
-        fscanf(fp, "%[^#]#%[^#]#%ld\n", ttemp, ntemp, &view[i]);
-        name[i] = malloc(1010*sizeof(char));
-        strcpy(name[i], ntemp);
-        title[i] = malloc(1010*sizeof(char));
-        strcpy(title[i], ttemp);
-        // printf("%s %s %ld", title[i], name[i], view[i]);
+        fscanf(fp, "%[^#]#%lf#%c\n", ntemp, &temperature[i], &type[i]);
+        town[i] = malloc(1010*sizeof(char));
+        strcpy(town[i], ntemp);
+        if(type[i] == 'F'){
+            sort[i] = (temperature[i] - 32) * 5 / 9;
+        } else sort[i] = temperature[i];
         i++;
     }
+
     for (int j = 0; j < i; j++){
         for (int k = 0; k < i - j - 1; k++){
-            if (view[k] < view[k + 1]){
-                swap_str(name + k, name + k + 1);
-                swap_str(title + k, title + k + 1);
-                swap(&view[k], &view[k + 1]);
-            } else if(view[k] == view[k + 1]){
-                swap_str(name + k, name + k + 1);
-                swap_str(title + k, title + k + 1);
+            if (sort[k] > sort[k + 1]){
+                swap_str(town + k, town + k + 1);
+                swap_chr(&type[k], &type[k + 1]);
+                swap(&temperature[k], &temperature[k + 1]);
+                swap(&sort[k], &sort[k + 1]);
+            } else if(sort[k] == sort[k + 1]){
+                swap_str(town + k, town + k + 1);
+                swap_chr(&type[k], &type[k + 1]);
             }
         }
     }
-
+    // puts("");
+    // for(int j = 0; j < i; j++){
+    //     printf("%f\n", sort[j]);
+    // }
+    // puts("");
     for(int d = 0; d < i; d++){
-        printf("%s by %s - %ld\n", title[d], name[d], view[d]);
+        printf("%s is %.2f%c\n", town[d], temperature[d], type[d]);
     }
     // puts("");
     fclose(fp);
