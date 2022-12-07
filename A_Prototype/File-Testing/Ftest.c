122853b466b7379e91607a93e612d45d12e70118
Eilhie <marvinsachio89@gmail.com>
2022-12-07 13:39:28 +0700

    SOCS

--- A_Prototype/File-Testing/Ftest.c
+++ A_Prototype/File-Testing/Ftest.c
@@ -1,51 +1,40 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void swap(char *x, char *y){
-    char temp = *x;
-    *x = *y;
-    *y = temp;
-} 
-
-int main(){    
-    FILE *fp;
-    fp = fopen("testdata.in","r");
-    int cases;
-    fscanf(fp, "%d\n", &cases);
-
-    for(int i = 0; i < cases; i++){
-        char str[111];
-        int changes;
-        char search, new[30];
-        int changed[100] = {0};
-        int count[26] = {0};
-        fscanf(fp, "%s\n", str);
-        fscanf(fp, "%d\n", &changes);
-        for(int j = 0; j < changes; j++){
-            fscanf(fp, "%c %c\n", &search, &new[j]);
-            for(int k = 0; k < strlen(str); k++){
-                if((str[k] == search) && !changed[k]){
-                    str[k] = new[j];
-                    changed[k] = 1;
-                    count[k]++;
-                } else if(str[k] && !changed[k]){
-                    count[k]++;
-                }
+int main(){  
+    int T;
+    FILE *file = fopen("testdata.in", "r");  
+    fscanf(file, "%d\n", &T);     
+    for(int i=0; i<T; i++){ 
+        int changed[26] = {0};   
+        char string[100];     
+        int N;         
+        fscanf(file, "%s\n", string);      
+        fscanf(file, "%d\n", &N);    
+        int len = strlen(string);        
+        for(int j=0; j<N; j++){                          
+            char from;            
+            char to;                         
+            fscanf(file, "%c %c\n", &from, &to);   
+            if(!changed[from-'A']){
+                changed[from-'A']=1;
+                for (int k = 0; k < len; k++){                                              
+                    if (string[k] == from){                         
+                        string[k] = to;
+                    }                 
+                } 
             }
-        }
-        
-        int len = strlen(str);
-        for(int j = 0; j < len - 1; j++){
-            for(int k = 0; k < len - 1 -j; k++){
-                if(str[k] > str[k + 1]) swap(&str[k], &str[k + 1]);
-            }
-        }
-        for(int j = 0; j < len; j++){
-            printf("%c ", str[j]);
-        }
-        puts("");
-    }
-    fclose(fp);
-    return 0;
+        }             
+        int frequency[26] = {0};        
+        for (int j = 0; j < len; j++){                             
+            frequency[string[j] - 'A']++;            
+        }             
+        for (int j = 0; j < 26; j++){                            
+            if(frequency[j]){
+                printf("%c %d\n", 'A'+j, frequency[j]);
+            }          
+        }         
+    }     
+    return 0; 
 }
