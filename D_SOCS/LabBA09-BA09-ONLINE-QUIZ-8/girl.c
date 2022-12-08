0da4053b665a4a515130130b6e01d0bd6b8524b1
Eilhie <marvinsachio89@gmail.com>
2022-12-08 12:37:39 +0700

    SOCS

--- D_SOCS/LabBA09-BA09-ONLINE-QUIZ-8/girl.c
+++ D_SOCS/LabBA09-BA09-ONLINE-QUIZ-8/girl.c
@@ -1,77 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
-void swap(char *x, char *y){
-    char temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-int partition(char array[], int low, int high){
-    int i = low;
-
-    for(int j=low; j<high; j++){
-        if(array[j]<=array[high]){
-            swap(&array[i], &array[j]);
-            i++;
-        }
-    }
-
-    swap(&array[i], &array[high]);
-    
-    return i;
-}
-
-void quicksort_recursion(char array[], int low, int high){
-    if (low<high){
-        int pivotIndex = partition(array, low, high);
-        quicksort_recursion(array, low, pivotIndex-1);
-        quicksort_recursion(array, pivotIndex + 1, high);
-    }
-}
-
-void quicksort(char array[], int length){
-
-    quicksort_recursion(array, 0, length-1);
-}
-
-void swap_str(char **x, char **y){
-    char *temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
+#include <stdlib.h>
 
 int main(){
 
     int testCases;
     scanf("%d", &testCases);
     for(int i = 0; i < testCases; i++){
-        char *string = (char *) malloc (sizeof(char) * (int) pow(10, 6)); 
-        scanf("%s", string);
+        char *s = (char *) malloc (sizeof(char) * (int) pow(10, 6)); 
+        scanf("%s", s);
+        int alpha[26] = {0};
         int result = 0;
-        quicksort(string, strlen(string));
-        printf("%s\n", string);
-        for(int k = 0; k < strlen(string) ; k++){
-            int count = 1;
-            
-            for(int j = k + 1 ; j < strlen(string) - 1; j++){    
-                if(string[k] == string[j]){
-                    count++;
-                }
-            }  
-
-            if(count > 0) result++;
-        } 
+        for(int i = 0; i < strlen(s); i++){
+            alpha[s[i] - 'a']++;
+        }
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] > 0) result++;
+        }
         if(result % 2 == 0)
             printf("Case #%d: Yay\n", i + 1);
         else 
             printf("Case #%d: Ewwww\n", i + 1);
-
-        printf("%s %d\n", string, result);
     }
 
     return 0;
 }
+
+// int main() {
+//     int t, counter;
+//     bool distinct[27];
+//     scanf("%d", &t);
+//     char username[t+1][120000];
+//     for (int i = 1; i <= t; i++) {
+//         for (int j = 0; j < 26; j++) {
+//             distinct[j] = false;
+//         }
+//         scanf("%s", username[i-1]);
+//         for (int j = 0; username[i-1][j] != '\0'; j++) {
+//             distinct[username[i-1][j]-'a'] = true;
+//         }
+//         counter = 0;
+//         for (int j = 0; j < 26; j++) {
+//             if (distinct[j] == true) counter++;
+//         }
+//         printf("Case #%d: %s\n", i, (counter%2 == 0)?"Yay":"Ewwww");
+//     }
+//     return 0;
+// }
