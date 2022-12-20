0b6334ac69a8f840a38c9acaec2d5460df37844a
Eilhie <marvinsachio89@gmail.com>
2022-12-20 07:33:44 +0700

    Forum

--- B_AlgoProg/Forum/savenload/saveandload.c
+++ B_AlgoProg/Forum/savenload/saveandload.c
@@ -4,7 +4,9 @@
 
 int filecheck(const char *file_name);
 int confirmation(char x);
+void savemode();
 void save();
+void loadmode();
 void load();
 void MainMenu();
 
@@ -36,19 +38,102 @@ int confirmation(char x){
     return -1;
 }
 
-void save(){
-    FILE *fp = fopen("savedata.in", "a");
+void savemode(){
+    FILE *fp = fopen("savedata.in", "a+");
+    int line_count = 0;
+    int conf;
+    char x;
+    char buffer[1024];
 
-    system("cls");
-    printf("SAVE\n");
-    printf("-------------------------------------\n");
+    while (fgets(buffer, sizeof(buffer), fp) != NULL){
+        line_count++;
+    }
+    do{
+        printf("Type some text to be saved. . .\n > ");
+        scanf("%[^\n]", buffer); getchar();
+        fprintf(fp, "%d %s\n", line_count + 1, buffer);
+        printf("do you want to save another text? (Y/n)\n > ");
+        scanf("%c", &x); getchar();
+        conf = confirmation(x);
+        if(conf == 0) break;
+    } while(conf < 0);
     
-
+    system("pause");
+    MainMenu();
+    
     fclose(fp);
 }
 
+void save(){
+    char x;
+    int conf;
+    int nums = 0;
+    system("cls");
+    printf("SAVE\n");
+    printf("-------------------------------------\n");
+    if(filecheck("savedata.in")) {
+        printf("There is no saved data!\n");
+    } else 
+        printf("Saved data Found!\n");    
+
+    do{
+        printf("do you want to add data to file? (Y/n)\n > ");
+        scanf("%c", &x); getchar();
+        conf = confirmation(x);
+    } while(conf < 0); 
+    if(conf == 1) savemode();
+    else MainMenu();
+
+    system("pause");
+    MainMenu();
+}
+
+void loadmode(){
+    FILE *fp = fopen("savedata.in", "r");
+    int line_count = 0;
+    int conf, loaded;
+    char x;
+    char buffer[1024];
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL){
+        line_count++;
+    }
+    for(int i = 1; i <= line_count; i++){
+        if(i % 5 == 0) printf(" %d\n", i);
+        else printf(" %d ", i);
+    }
+    puts("");
+    do{
+        do{
+            printf("Select Data to be Loaded\n > ");
+            scanf("%d", &loaded); getchar();
+        }while(loaded < 0 || loaded > line_count);
+
+        int count = 0;
+        while (fgets(buffer, sizeof(buffer), fp) != NULL){
+            count++;
+            if (count == loaded){
+                fgets(buffer, sizeof(buffer), fp);
+                break;
+            }
+        }
+        printf("%s\n", buffer);
+    
+        if(line_count > 1){
+            printf("do you want to Load another text? (Y/n)\n > ");
+            scanf("%c", &x); getchar();
+            conf = confirmation(x);
+            if(conf == 0) break;
+        } else break;
+    } while(conf < 0);
+
+    fclose(fp);
+    system("pause");
+    MainMenu();
+}
+
 void load(){
-    FILE *fp = fopen("savedata.in", "a");
+    
     char x;
     int conf;
 
@@ -67,10 +152,18 @@ void load(){
         else MainMenu();
     }
     else {
-        printf("file is not empty\n");
+        printf("Saved data found\n");
+
+        do{
+            printf("do you want to load the data? (Y/n)\n > ");
+            scanf("%c", &x); getchar();
+            conf = confirmation(x);
+        } while(conf < 0); 
+        if(conf == 1) loadmode();
+        else MainMenu();
     }
+    system("pause");
     MainMenu();
-    fclose(fp);
 }
 
 void MainMenu(){
