b01ec9741974e89f135cec432fdae9da996057d5
Eilhie <marvinsachio89@gmail.com>
2022-12-30 15:45:05 +0700

    Prototype

--- A_Prototype/TestFiles/string.c
+++ A_Prototype/TestFiles/string.c
@@ -3,16 +3,8 @@
 
 int main(){
 
-    char string[100] = "krisna";
-    char str[50] = "gay";
-
-    strcat(string, str);
+    char string[100] = "    krisna";
     printf("%s\n", string);
-
-
-    char temp[50];
-    strcpy(temp, string);
-    printf("%s\n", temp);
 
     return 0;
 }
