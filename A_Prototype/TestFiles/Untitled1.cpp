f09c94bba0e3d400c046cd43026b172f5d975895
Eilhie <marvinsachio89@gmail.com>
2023-01-23 20:07:04 +0700

    Prototype

---
+++ A_Prototype/TestFiles/Untitled1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main() {
+    Beep(659, 500);
+    Beep(659, 500);
+    Beep(659, 500);
+    Beep(523, 350);
+    Beep(659, 150);
+    Beep(784, 500);
+    Beep(392, 350);
+    Beep(523, 150);
+    Beep(659, 1000);
+    Beep(392, 350);
+    Beep(523, 150);
+    Beep(659, 1000);
+    Beep(784, 350);
+    Beep(392, 150);
+    Beep(523, 500);
+    Beep(392, 350);
+    Beep(523, 150);
+    Beep(659, 1000);
+    return 0;
+}
+
