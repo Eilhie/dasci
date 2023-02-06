e001bfbbdf1e3216e92298bef4669b26cb9811b2
Eilhie <marvinsachio89@gmail.com>
2023-02-06 20:46:14 +0700

    Semester One

--- G_Temporary/CSS Responsive + JS Basic/Real/css-responsive/script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector(".toggle-button");
-const navbarLinks = document.querySelector(".navbar-links");
-
-toggleBtn.addEventListener("click", () => {
-	navbarLinks.classList.toggle("navbar-links-mobile");
-});
