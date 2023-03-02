d9abf539c85782e0e5579d6b1edcce4e42b7ea27
Eilhie <marvinsachio89@gmail.com>
2023-03-02 11:34:37 +0700

    Semester_Two

--- SEMESTER_2/A_DataStructure/linkedlist/singlelinkedlist.cpp
+++ SEMESTER_2/A_DataStructure/linkedlist/singlelinkedlist.cpp
@@ -1,53 +1,100 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// struct Node{
+// 	// isinya data
+// 	int data;
+	
+// 	Node *next;
+	
+// };
+
+// Node *head = NULL;
+// Node *tail = NULL;
+
+// // function to allocate memory
+// Node* createNode(int data) {
+// 	Node *newNode = (Node*) malloc(sizeof(Node));
+// 	newNode->data = data;
+// 	newNode->next = NULL;
+// 	return newNode;
+// }
+
+// // push head -> insert ke kepala
+// void pushHead(int data) {
+	
+// 	Node *newNode = createNode(data);
+// 	if(head == NULL) {
+// 		head = newNode;
+// 		tail = newNode;
+// 	} else {
+// 		newNode->next = head;
+// 		head = newNode;
+// 	}
+// }
+
+
+// void printList() {
+	
+// 	while (head != NULL) {
+// 		printf("%d ->", head->data);
+// 		head = head->next;
+// 	}
+// 	puts("");
+// }
+
+// int main() {
+// 	pushHead(10);
+// 	pushHead(20);
+// 	pushHead(30);
+	
+// 	printList();
+// 	return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct Node{
-	// isinya data
-	int data;
+struct Book {
+	int price;
+	Book *next;
+} *head, *tail;
+
+
+Book* createNode(int price) {
+	Book *temp = (Book*)malloc(sizeof(Book));
+	temp->price = price;
 	
-	Node *next;
-	
-};
+	temp->next = NULL;
 
-Node *head = NULL;
-Node *tail = NULL;
-
-// function to allocate memory
-Node* createNode(int data) {
-	Node *newNode = (Node*) malloc(sizeof(Node));
-	newNode->data = data;
-	newNode->next = NULL;
-	return newNode;
+	return temp;
 }
 
-// push head -> insert ke kepala
-void pushHead(int data) {
-	
-	Node *newNode = createNode(data);
-	if(head == NULL) {
-		head = newNode;
-		tail = newNode;
+void pushHead(int price) {
+	Book *node = createNode(price);
+
+	if(head == NULL){
+		head = tail = node;
 	} else {
-		newNode->next = head;
-		head = newNode;
+		node->next = head;
+		head = node;
 	}
 }
 
-
-void printList() {
-	
-	while (head != NULL) {
-		printf("%d ->", head->data);
-		head = head->next;
+void printLL(){
+	Book* curr = head;
+	while(curr != NULL){
+		printf("%d\n", curr->price);
+		curr = curr->next;
 	}
-	puts("");
 }
 
-int main() {
-	pushHead(10);
-	pushHead(20);
-	pushHead(30);
+int main(){
+	pushHead(100);
+	pushHead(500);
+	pushHead(750);
 	
-	printList();
+	printLL();
 	return 0;
 }
