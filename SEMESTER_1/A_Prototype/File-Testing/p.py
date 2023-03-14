217717e9e46bd6bad7c2b37f36fd67a2a160f29f
Eilhie <marvinsachio89@gmail.com>
2023-03-15 01:44:27 +0700

    DSC

---
+++ SEMESTER_1/A_Prototype/File-Testing/p.py
@@ -0,0 +1,44 @@
+from IPython.display import clear_output
+import time
+import turtle
+
+print("Ayaaaaaaa :<")
+time.sleep(2)
+clear_output(wait=True)
+print("\nAku buat kayak gini boleh kan :< ?")
+# Clear the output
+clear_output(wait=True)
+print("Kina parah banget hari ini pake banget, ya karena kina juga sih hari ini asya jadi begini\n")
+time.sleep(2)
+print("Maapin kina for today kalau banyak ngecewain asya\n")
+time.sleep(2)
+print("Kina pengen asya jangan stress, makan yang banyak, sama banyak-banyak sayang kinaaa")
+time.sleep(2)
+print("Kina Pengen Disayang sama asyaa pake bangett")
+time.sleep(2)
+clear_output(wait=True)
+print("Pengen di pat-pat")
+time.sleep(2)
+clear_output(wait=True)
+print("Pengen di peyukkk")
+time.sleep(2)
+clear_output(wait=True)
+print("Pengen juga di ituin >_<")
+time.sleep(2)
+clear_output(wait=True)
+print("Udah mungkin gitu ajaa pokoknya kina loves youuu poreperrr + jangan di diemin lagiii Huwaaaa :< \n\n")
+heart = "   ___   ___\n" \
+        " /     \/    \ \n" \
+        "|             |\n" \
+        "|             |\n" \
+        "|             |\n" \
+        " \           /\n" \
+        "  \         /\n" \
+        "   \       /\n" \
+        "    \     /\n" \
+        "     \   /\n" \
+        "      \ /\n" \
+
+
+print(heart)
+print("\n\n Sumpah simetrissinnya susah")
