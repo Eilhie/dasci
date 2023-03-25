c1968fbc9804f473b2e3e7604c79b9b7f1919954
Eilhie <marvinsachio89@gmail.com>
2023-03-25 14:15:56 +0700

    hackerrank

---
+++ HackerRank/miniMaxSum.c
@@ -0,0 +1,192 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void swap(long long int* a, long long int *b){
+    long long int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubleSort(int arr_c, long long int* arr){
+    for(int i = 0; i < arr_c; i++){
+        for(int j = 0; j < arr_c - i - 1; j++){
+            if(arr[j] < arr[j + 1]){
+                swap(&arr[j], &arr[j + 1]);
+            } 
+        }
+    }
+}
+
+void miniMaxSum(int arr_count, int* arr) {
+    long long int* sumarr;
+    sumarr = calloc(arr_count, sizeof(long long));
+    long long int sum = 0;
+    for (int i = 0; i < arr_count; i++) {
+        for(int j = 0; j < arr_count; j++){
+            if(j != i) sum += arr[j];
+        }
+        sumarr[i] = sum;
+        sum = 0;
+    }
+    bubleSort(arr_count, sumarr);
+    printf("%lld %lld\n", sumarr[arr_count - 1], sumarr[0]);
+}
+
+int main()
+{
+
+    char** arr_temp = split_string(rtrim(readline()));
+
+    int* arr = malloc(5 * sizeof(int));
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
+
+        *(arr + i) = arr_item;
+    }
+
+    miniMaxSum(5, arr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
